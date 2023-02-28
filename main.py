c152ccb8051f3d006f0e7ca116283d627a0e4542
1aftermagnacarta <molt3nyt@gmail.com>
2023-02-28 00:09:20 +0000

    added comments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
 import time
+
 directions = ["Left","Right","Up","Down","Forward"]
 
-while True:
+while True: #infinitely loops through directions.
     ranTime = random.randint(30,60)
     ranDirection = random.randint(0,len(directions)-1)
     print(directions[ranDirection])
