c7a46e93fec3eeec2d843a1692d734a018647f4e
1aftermagnacarta <molt3nyt@gmail.com>
2023-02-28 00:04:30 +0000

    fixed basic logic error

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ directions = ["Left","Right","Up","Down","Forward"]
 while True:
     ranTime = random.randint(30,60)
     ranDirection = random.randint(0,len(directions)-1)
-    time.sleep(ranTime)
     print(directions[ranDirection])
+    time.sleep(ranTime)
