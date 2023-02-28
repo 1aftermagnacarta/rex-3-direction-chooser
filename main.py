28efbc94bd540b262b81d9962d9921f1ae9b067f
1aftermagnacarta <125931105+1aftermagnacarta@users.noreply.github.com>
2023-02-28 11:44:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import time
 
 directions = ["Left","Right","Up","Down","Forward"]
 
+sameVal = input("Duplicate directions? Y/N \n")
+if sameVal.lower() == "n":
+    sameVal = False
+else:
+    sameVal = True
+
+count = 0
+prevDirection = ""
 while True: #infinitely loops through directions.
-    ranTime = random.randint(30,60)
-    ranDirection = random.choice(directions)
-    print(ranDirection)
+    ranTime = random.randint(3,6)
+    ranDirection = random.randint(0,len(directions)-1)
+    if count > 0:
+        if sameVal == False:
+            if prevDirection == ranDirection:
+                if prevDirection < len(directions)-1:
+                    ranDirection = random.randint(prevDirection+1,len(directions)-1)
+                else:
+                    ranDirection = random.randint(0,prevDirection-1)
+    prevDirection = ranDirection
+    print("\n",directions[ranDirection])
     time.sleep(ranTime)
+    count += 1
