368d3060f8697e08db10504d527b4eda9589e844
1aftermagnacarta <125931105+1aftermagnacarta@users.noreply.github.com>
2023-03-02 08:40:02 +0000

    Update main.py
    Removed unnecessary variables.

--- main.py
+++ main.py
@@ -11,19 +11,16 @@ directions = ["Left","Right","Up","Down","Forward"]
 
 sameVal = toBool(input("Duplicate directions? Y/N \n"))
 
-count = 0
 prevDirection = ""
 while True: #infinitely loops through directions.
     ranTime = random.randint(30,60)
     ranDirection = random.randint(0,len(directions)-1)
-    if count > 0:
-        if sameVal == False:
-            if prevDirection == ranDirection:
-                if prevDirection < len(directions)-1:
-                    ranDirection = random.randint(prevDirection+1,len(directions)-1)
-                else:
-                    ranDirection = random.randint(0,prevDirection-1)
+    if sameVal == False:
+        if prevDirection == ranDirection:
+            if prevDirection < len(directions)-1:
+                ranDirection = random.randint(prevDirection+1,len(directions)-1)
+            else:
+                ranDirection = random.randint(0,prevDirection-1)
     prevDirection = ranDirection
     print("\n",directions[ranDirection])
     time.sleep(ranTime)
-    count += 1
